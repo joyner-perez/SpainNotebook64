d641feab385c511d8682b940507b9902d746c7f6
Joyner Pérez Echevarria <joynerechevarria@gmail.com>
2026-06-02 09:26:51 +0200

    Add navigation bars padding to ParseScreen layout

--- app/src/main/kotlin/com/joyner/notebook64/ParseScreen.kt
+++ app/src/main/kotlin/com/joyner/notebook64/ParseScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
@@ -104,7 +105,7 @@ fun ParseScreen(modifier: Modifier = Modifier) {
         }
 
     Scaffold(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize().navigationBarsPadding(),
         bottomBar = {
             Button(
                 modifier =
